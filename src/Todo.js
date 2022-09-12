7575235903c81b2d0716a58faa4cf374f5ba0fd2
DanielleM <dlmedeiro@gmail.com>
2022-09-12 12:25:15 -0700

    remove react state

--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Todo = ({id, todo, remove}) => {
+const Todo = ({ id, todo, remove }) => {
+  const handleRemove = () => {
+    remove(id);
+  };
 
-    const handleRemove = () => {
-        remove(id)
-    }
-
-    return (
-        <div>
-            <li id={id}>
-                {todo}
-                <button onClick={handleRemove}>Remove Todo</button>
-            </li>
-        </div>
-    )
-}
+  return (
+    <div>
+      <li id={id}>
+        {todo}
+        <button onClick={handleRemove}>Remove Todo</button>
+      </li>
+    </div>
+  );
+};
 
 export default Todo;
