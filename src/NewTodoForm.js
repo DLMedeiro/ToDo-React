e21d577c8e701658dc9fc993d41a1ece582c1c33
DanielleM <dlmedeiro@gmail.com>
2022-09-12 14:08:17 -0700

    implement uuid close #2

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,52 +1,53 @@
 import React, { useState } from "react";
+import { v4 } from "uuid";
 
-const NewTodoForm = ({addItem}) => {
-    const INITIAL_STATE = {
-        id: '',
-        todo: ''
-    }
+const NewTodoForm = ({ addItem }) => {
+  const INITIAL_STATE = {
+    id: "",
+    todo: "",
+  };
 
-    const [formData, setFormData] = useState(INITIAL_STATE);
+  const [formData, setFormData] = useState(INITIAL_STATE);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(formData => ({
-            ...formData,
-            [name]: value
-        }))
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addItem(formData.id, formData.todo)
-        setFormData(INITIAL_STATE);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(v4(), formData.todo);
+    setFormData(INITIAL_STATE);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='id'>id</label>
-            <input
-                id='id'
-                type = 'text'
-                name = 'id'
-                placeholder = 'id'
-                value = {formData.id}
-                onChange = {handleChange}
-            />
-            <label htmlFor='todo'>todo</label>
-            <input
-                id='todo'
-                type = 'text'
-                name = 'todo'
-                placeholder = 'todo'
-                value = {formData.todo}
-                onChange = {handleChange}
-            />
-            <p>{formData.id}</p>
-            <p>{formData.todo}</p>
-            <button>Add Todo Item</button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* <label htmlFor="id">id</label>
+      <input
+        id="id"
+        type="text"
+        name="id"
+        placeholder="id"
+        value={formData.id}
+        onChange={handleChange}
+      /> */}
+      <label htmlFor="todo">todo</label>
+      <input
+        id="todo"
+        type="text"
+        name="todo"
+        placeholder="todo"
+        value={formData.todo}
+        onChange={handleChange}
+      />
+      {/* <p>{formData.id}</p> */}
+      <p>{formData.todo}</p>
+      <button>Add Todo Item</button>
+    </form>
+  );
+};
 
 export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
-import NewTodoForm from './NewTodoForm';
-import Todo from './Todo';
+import NewTodoForm from "./NewTodoForm";
+import Todo from "./Todo";
+import { v4 } from "uuid";
 
 const TodoList = () => {
-    const INITIAL_STATE = [
-        {id:1, todo: "Learn React"},
-        {id: 2, todo: "Clean Kitchen"}
-    ]
+  const INITIAL_STATE = [
+    { id: v4(), todo: "Learn React" },
+    { id: v4(), todo: "Clean Kitchen" },
+  ];
 
-    const [todos, setTodos] = useState(INITIAL_STATE);
-    const addItem = (id, todo) => {
-        setTodos(todos => [...todos, {id, todo}])
-    }
-    const remove = (id) => {
-        setTodos(todos.filter(t => t.id !==id))
-    }
+  const [todos, setTodos] = useState(INITIAL_STATE);
+  const addItem = (id, todo) => {
+    setTodos((todos) => [...todos, { id, todo }]);
+  };
+  const remove = (id) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
 
-    return (
-        <div>
-            <NewTodoForm addItem={addItem}/>
-            <div>
-                {todos.map(({id, todo}) => <Todo id={id} todo={todo} remove={remove}/>)}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NewTodoForm addItem={addItem} />
+      <div>
+        {todos.map(({ id, todo }) => (
+          <Todo id={id} todo={todo} remove={remove} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
